8ae4c609a72d3d79dd32b27ede730208dee2cd03
ipalma <isaac.palma@tajamar365.com>
2018-05-25 13:48:41 +0200

    Actualizaciones bbdd

--- SQL Server DATABASE/Address.sql
+++ SQL Server DATABASE/Address.sql
@@ -1,8 +1,16 @@
 ﻿CREATE TABLE [dbo].[Address]
 (
 	[AddressId] INT NOT NULL PRIMARY KEY IDENTITY(1, 20), 
-    [AddressLine1] NVARCHAR(150) NOT NULL, 
+    [AddressLine1] NVARCHAR(150) NOT NULL,
+	[NumberLine1] NVARCHAR(10) NULL,
+	[FloorLine1] NVARCHAR(10) NULL,
+	[DoorLine1] NVARCHAR(10) NULL,
+	[StairLine1] NVARCHAR(10) NULL,
     [AddressLine2] NVARCHAR(150) NULL, 
+	[NumberLine2] NVARCHAR(10) NULL,
+	[FloorLine2] NVARCHAR(10) NULL,
+	[DoorLine2] NVARCHAR(10) NULL,
+	[StairLine2] NVARCHAR(10) NULL,
     [City] INT NOT NULL, 
     [PostalCode] NVARCHAR(15) NOT NULL, 
     [rowguid] UNIQUEIDENTIFIER NOT NULL, 
--- SQL Server DATABASE/City.sql
+++ SQL Server DATABASE/City.sql
@@ -2,8 +2,9 @@
 (
 	[CityId] INT NOT NULL PRIMARY KEY IDENTITY, 
     [ProvinceId] INT NOT NULL, 
-    [CityName] INT NOT NULL, 
+    [CityName] NVARCHAR(150) NOT NULL, 
     [rowguid] UNIQUEIDENTIFIER NOT NULL, 
     [ModifiedDate] DATETIME NOT NULL, 
+    [DC] INT NULL, 
     CONSTRAINT [FK_City_Province] FOREIGN KEY ([ProvinceId]) REFERENCES [Province]([ProvinceId])
 )
